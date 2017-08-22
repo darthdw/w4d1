c0c62515ab82a4510f76dc08bcbb085ac50fc4ab
darthdw <thebackupname@gmail.com>
2017-08-21 17:08:11 -0700

    routes project

---
+++ routes_project/app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+  # COMMENTED OUT FOR THIS PROJECT ONLY. I feel like Kelly Jr.
+end
---
+++ routes_project/app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save!
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: @user
+    else
+      render text: "User not found."
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: @user
+  end
+
+  def update
+    @user = User.find(user_params)
+    if @user
+      @user.update(user_params)
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].permit(:name, :email)
+  end
+
+end
